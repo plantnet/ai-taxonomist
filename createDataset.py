0325200cef0555bbef9383f7b070bc0f45783b53
Jean-Christophe Lombardo <jean-christophe.lombardo@inria.fr>
2021-08-12 16:52:42 +0200

    #1 add a --doi option to create a dataset from a gbif query doi

--- createDataset.py
+++ createDataset.py
@@ -61,8 +61,8 @@ def main():
                         help='json file listing gbif species ids')
     intermediate.add_argument('--providers', type=argparse.FileType('r'),
                         help='json file listing gbif providers ids')
-    # expert = parser.add_argument_group(title='expert', description='requires to fully understand gbif')
-    # expert.add_argument('--doi', type=str, help="a gbif's query doi")
+    expert = parser.add_argument_group(title='expert', description='requires to fully understand gbif')
+    expert.add_argument('--doi', type=str, help="a gbif's query doi")
 
     args = parser.parse_args()
 
@@ -70,27 +70,40 @@ def main():
 
     species_ids = []
     if args.crawl:
-        if args.names:
-            if args.species or args.doi:
-                print('it does not make sens to provide both --name and --species or --doi arguments')
-                sys.exit(-1)
-            species_ids = crawlNames(args)
-        elif args.species:
-            species_ids = json.load(args.species)
-        #handle doi
+        if not args.doi:
+            if args.names:
+                if args.species:
+                    print('it does not make sens to provide both --name and --species arguments')
+                    sys.exit(-1)
+                species_ids = crawlNames(args)
+            elif args.species:
+                species_ids = json.load(args.species)
+            #handle doi
 
-        urls = []
-        if args.providers:
-            providers = json.load(args.providers)
-            queries = {'speciesKey': species_ids, 'datasetKey': providers}
-        else:
-            queries = {'speciesKey': species_ids}
-        data_generator = gbif_dl.api.generate_urls(queries=queries, label='speciesKey', nb_samples_per_stream=args.number,
-                                                   split_streams_by=['speciesKey'], mediatype='StillImage', verbose=args.verbose)
+            if args.providers:
+                providers = json.load(args.providers)
+                queries = {'speciesKey': species_ids, 'datasetKey': providers}
+            else:
+                queries = {'speciesKey': species_ids}
+            data_generator = gbif_dl.api.generate_urls(queries=queries, label='speciesKey',
+                                                       nb_samples_per_stream=args.number,
+                                                       split_streams_by=['speciesKey'],
+                                                       mediatype='StillImage',
+                                                       one_media_per_occurrence=False,
+                                                       verbose=args.verbose)
+        else: # use a precomputed gbif query from its doi
+            if args.names or args.species:
+                print('it does not make sens to provide both --doi and --name or --species arguments')
+                sys.exit(-1)
+            data_generator = gbif_dl.dwca.generate_urls(args.doi,
+                                                        dwca_root_path=os.path.join(args.data, "dwcas"),
+                                                        label="speciesKey",
+                                                        mediatype='StillImage',
+                                                        one_media_per_occurrence=False,)
+
         print('Retrieving image metadata...')
+        urls = []
         for i in data_generator:
-            if args.verbose:
-                print(i)
             urls.append(i)
             if len(urls) % 500 == 0:
                 print(len(urls),'image metadata retrieved')
@@ -106,7 +119,7 @@ def main():
         img_dir = os.path.join(args.data, 'img')
         print("Downloading images to", img_dir)
         gbif_dl.io.download(urls, root=img_dir, random_subsets={'train':0.95, 'val':0.05},
-                            nb_workers=1, retries=1, verbose=True)
+                            nb_workers=1, retries=1, verbose=args.verbose)
         print('Images downloaded')
     else:
         print('Download skipped')
---
+++ frogs/expert/README.md
@@ -0,0 +1,6 @@
+We've prepared a gbif query to retrieve occurencies on genus Rana from trusted publishers: `https://doi.org/10.15468/dl.bx6xeq`
+
+Just try
+```
+python createDatabase.py --data /path/to/data --doi 10.15468/dl.bx6xeq
+```
